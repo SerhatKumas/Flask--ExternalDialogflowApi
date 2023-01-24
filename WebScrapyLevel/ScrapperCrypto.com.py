50dde79257079e75ccae77cde5b58a72d34eaadf
Serhat Kumas <serhatkumas@outlook.com>
2023-01-25 00:50:51 +0300

    All anchors that are under main categories are reached

--- WebScrapyLevel/ScrapperCrypto.com.py
+++ WebScrapyLevel/ScrapperCrypto.com.py
@@ -2,9 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://help.crypto.com/en/"
+all_main_page_anchors = []
+all_in_page_anchors = []
+one_page_anchors = []
 
-web_content = requests.get(url)
-soup = BeautifulSoup(web_content.content, features="lxml")
+content = requests.get(url)
+soup = BeautifulSoup(content.content, features="lxml")
 
-for a in soup.find_all('a', href=True):
-    print("Found the URL:", a['href'])
+for main_page_anchors in soup.find_all('a', href=True):
+    all_main_page_anchors.append("https://help.crypto.com"+main_page_anchors['href'])
+
+all_main_page_anchors = all_main_page_anchors[2:len(all_main_page_anchors) - 4]
+
+for anchor in all_main_page_anchors:
+    content = requests.get(anchor)
+    soup = BeautifulSoup(content.content, features="lxml")
+    for in_page_anchors in soup.find_all('a', href=True):
+        one_page_anchors.append("https://help.crypto.com"+in_page_anchors['href'])
+    all_in_page_anchors.append(one_page_anchors[2:len(one_page_anchors) - 4])
+
+print(str(all_in_page_anchors))
