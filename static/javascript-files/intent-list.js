36cc948be326a57b6013378ea536fbeb7639c834
Serhat Kumas <serhatkumas@outlook.com>
2023-02-05 19:43:40 +0300

    Intent response is added to intent page

--- static/javascript-files/intent-list.js
+++ static/javascript-files/intent-list.js
@@ -1,20 +1,19 @@
 function fetch_intent_list(){
-    fetch('/get-all-intents').then(response => response.json()).then(function(intent_data){
-        for(let i = 0; i < intent_data.length; i++){
+    let counter = 1;
+    fetch('/all-intents-page-info').then(response => response.json()).then(function(intent_data){
+        for(let i = 0; i < intent_data.length; i+=2){
             let tr_object = document.createElement("tr");
             let th_object = document.createElement("th");
             th_object.setAttribute("scope", "row");
-            th_object.innerHTML = (i+1).toString() ;
+            th_object.innerHTML = (counter).toString() ;
+            counter++;
             let td_object_1 = document.createElement("th");
             td_object_1.innerHTML = intent_data[i] ;
             let td_object_2 = document.createElement("th");
-            td_object_2.innerHTML = "Training Name" ;
-            let td_object_3 = document.createElement("th");
-            td_object_3.innerHTML = "Response";
+            td_object_2.innerHTML = intent_data[i+1];
             tr_object.appendChild(th_object);
             tr_object.appendChild(td_object_1);
             tr_object.appendChild(td_object_2);
-            tr_object.appendChild(td_object_3);
             document.getElementById("information-table").appendChild(tr_object);
         }
     });
